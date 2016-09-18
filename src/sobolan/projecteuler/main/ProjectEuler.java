cb696f82664313ad054c4621cfe375fe4dfbf20c
Radu Murzea <radu.murzea@gmail.com>
2016-09-18 17:15:15 +0300

    Added code for problem 21

--- src/sobolan/projecteuler/main/ProjectEuler.java
+++ src/sobolan/projecteuler/main/ProjectEuler.java
@@ -63,6 +63,7 @@ public class ProjectEuler
         problemMap.put("18", ProjectEuler18.class);
         problemMap.put("19", ProjectEuler19.class);
         problemMap.put("20", ProjectEuler20.class);
+        problemMap.put("21", ProjectEuler21.class);
         problemMap.put("22", ProjectEuler22.class);
         problemMap.put("24", ProjectEuler24.class);
         problemMap.put("25", ProjectEuler25.class);

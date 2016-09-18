f956549f2fb6d2939a84b45a542d11ee7cea7217
Radu Murzea <radu.murzea@gmail.com>
2016-09-18 15:56:07 +0300

    Added code for problem 19

--- src/sobolan/projecteuler/main/ProjectEuler.java
+++ src/sobolan/projecteuler/main/ProjectEuler.java
@@ -58,5 +58,6 @@ public class ProjectEuler
         problemMap.put("13", ProjectEuler13.class);
         problemMap.put("16", ProjectEuler16.class);
         problemMap.put("17", ProjectEuler17.class);
+        problemMap.put("19", ProjectEuler19.class);
     }
 }

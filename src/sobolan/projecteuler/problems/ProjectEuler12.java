7913af41a472f71c2854cb50c986f62b83b63578
Radu Murzea <radu.murzea@gmail.com>
2016-09-24 14:25:37 +0300

    Dumped old implementation in favor of the use of Tau function for finding the number of divisors of a number.
    It's harder to code, but reduces computation time by about 60%.

--- src/sobolan/projecteuler/problems/ProjectEuler12.java
+++ src/sobolan/projecteuler/problems/ProjectEuler12.java
@@ -1,5 +1,8 @@
 package sobolan.projecteuler.problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author Radu Murzea
  *
@@ -21,6 +24,9 @@ package sobolan.projecteuler.problems;
 public class ProjectEuler12 extends AbstractExecutableProblem
 {
     private final int NR_MAX_DIVISORS = 500;
+    private final int MAX_VAL_OF_PRIMES = 1 * 1000 * 1000;
+    private boolean[] primes;
+    private int primeCursor = 0;
 
     @Override
     public String getResult()
@@ -28,39 +34,116 @@ public class ProjectEuler12 extends AbstractExecutableProblem
         int maxNrOfDivisors = 0;
         int triangle = 0;
 
+        this.generatePrimes();
+
         //we start at 5 because, from the problem statement, we know that the 5th triangle number has only 6 divisors
-        int i = 5;
-        while (true) {
+        for (int i = 5; i < Integer.MAX_VALUE; i++) {
             //calculate the triangle value
             triangle = (i * i + i) / 2;
 
-            //the triangle has 1 and itself as divisors, so we count those from the start
-            int nrOfDivisors = 2;
-
-            //to find how many divisors X has, find how many it has between 2 and SQRT(X) and then double it
-            int divisionLimit = (int) Math.sqrt(triangle);
-            for (int divisor = 2; divisor <= divisionLimit; divisor++) {
-                if (triangle % divisor == 0) {
-                    nrOfDivisors += 2;
-                }
+            /**
+             * If N can be written as N = p^a x q^b x r^c (where p, q and r are prime factors),
+             * then the number of divisors of N is (a + 1) x (b + 1) x (c + 1)
+             * This is known as the Tau function.
+             * @see http://mathschallenge.net/library/number/number_of_divisors
+             */
+            int[][] primeFactors = this.getPrimeFactors(triangle);
+            int nrOfDivisors = 1;
+            for (int[] primeFactor : primeFactors) {
+                nrOfDivisors *= (primeFactor[1] + 1);
             }
 
-            //subtract 1 if number is perfect square
-            if (divisionLimit * divisionLimit == triangle) {
-                nrOfDivisors--;
-            }
-
-            if (nrOfDivisors > maxNrOfDivisors) {
-                maxNrOfDivisors = nrOfDivisors;
-            }
-
-            if (maxNrOfDivisors > this.NR_MAX_DIVISORS) {
+            if (nrOfDivisors > this.NR_MAX_DIVISORS) {
                 break;
             }
-
-            i++;
         }
 
         return Integer.toString(triangle);
+    }
+
+    private void generatePrimes()
+    {
+        this.primes = new boolean[this.MAX_VAL_OF_PRIMES];
+
+        //primes[i] is true if the number i is a prime
+        //at first, let's assume every number is prime
+        Arrays.fill(this.primes, 0, this.MAX_VAL_OF_PRIMES, true);
+
+        //since we'll start with 2, let's mark the first 2 manually,
+        //so as not to disturb us in the loop :)
+        this.primes[0] = this.primes[1] = false;
+
+        //implementation of the Sieve of Eratosthenes
+        int limit = (int) Math.sqrt(this.MAX_VAL_OF_PRIMES) + 1;
+        for (int i = 2; i < limit; i++) {
+            if (this.primes[i]) {
+                for (int j = i * i; j < this.MAX_VAL_OF_PRIMES; j += i) {
+                    if (this.primes[j]) {
+                        this.primes[j] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    private int getNextPrimeFactor()
+    {
+        for (int i = this.primeCursor + 1; i < this.primes.length; i++) {
+            if (this.primes[i] == true) {
+                this.primeCursor = i;
+                return i;
+            }
+        }
+
+        if (this.primeCursor % 2 == 0) {
+            this.primeCursor++;
+        }
+
+        return this.primeCursor;
+    }
+
+    private void resetSieveCursor()
+    {
+        this.primeCursor = 0;
+    }
+
+    private int[][] getPrimeFactors(int x)
+    {
+        //check if x is a perfect square
+        int sqrtX = (int) Math.sqrt(x);
+        if (sqrtX * sqrtX == x) {
+            int[][] result = new int[1][2];
+            result[0][0] = sqrtX;
+            result[0][1] = 2;
+
+            return result;
+        }
+
+        this.resetSieveCursor();
+
+        ArrayList<Integer> factors = new ArrayList<>();
+        ArrayList<Integer> powers = new ArrayList<>();
+        int factor = this.getNextPrimeFactor();
+        while (x > 1) {
+            if (x % factor == 0) {
+                factors.add(factor);
+                int power = 0;
+                while (x % factor == 0) {
+                    x /= factor;
+                    power++;
+                }
+                powers.add(power);
+            }
+
+            factor = this.getNextPrimeFactor();
+        }
+
+        int[][] result = new int[factors.size()][2];
+        for (int i = 0; i < result.length; i++) {
+            result[i][0] = factors.get(i);
+            result[i][1] = powers.get(i);
+        }
+
+        return result;
     }
 }

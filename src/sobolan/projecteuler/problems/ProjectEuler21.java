7de4eb21a74d31f74883f562d714937e3746d3d9
Radu Murzea <radu.murzea@gmail.com>
2016-10-09 20:29:48 +0300

    A little speed-up

--- src/sobolan/projecteuler/problems/ProjectEuler21.java
+++ src/sobolan/projecteuler/problems/ProjectEuler21.java
@@ -20,11 +20,11 @@ public class ProjectEuler21 extends AbstractExecutableProblem
     {
         int amicableSum = 0;
         for (int a = 1; a < this.LIMIT; a++) {
-            int divisorsSum = this.getSumOfDivisors(a);
+            int divisorsSum = this.getProperDivisorsSum(a);
 
             //it's important to compare the number to its divisors-sum as the first member of the AND expression
             //this speeds up the execution speed by making good use of boolean short-circuit
-            if (a != divisorsSum && this.getSumOfDivisors(divisorsSum) == a) {
+            if (a != divisorsSum && this.getProperDivisorsSum(divisorsSum) == a) {
                 amicableSum += a;
             }
         }
@@ -32,15 +32,17 @@ public class ProjectEuler21 extends AbstractExecutableProblem
         return Integer.toString(amicableSum);
     }
 
-    private int getSumOfDivisors(int number)
+    private int getProperDivisorsSum(int number)
     {
+        int limit = (int) Math.round(Math.sqrt(number));
         int sum = 1;
 
-        int limit = (number / 2) + 1;
-
-        for (int i = 2; i < limit; i++) {
-            if (number % i == 0) {
+        for (int i = 2; i <= limit; i++) {
+            //the order of the 2 cases is important here, as the 2nd one is a more general case of the 1st one
+            if (i * i == number) {
                 sum += i;
+            } else if (number % i == 0) {
+                sum += (i + number / i);
             }
         }
 
